3b601330f404f9aeefe86c327294b136e584dee3
Lucía <lumes77iza@gmail.com>
2024-01-03 09:53:42 -0300

    adding html to read variables

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
+from flask import Flask, render_template, request
 import re
 import json
+
+app = Flask(__name__)
 
 def replace_variables(template, variables):
     for key, value in variables.items():
@@ -18,14 +21,26 @@ def read_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
-templates = ["00_app.yml", "01_service.yml", "02_ingress.yml"]
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        variables_json = request.form['variables']
+        variables = json.loads(variables_json)
 
-for template in templates:
-    yaml_file_path = f'./plantillas/{template}'
-    output_yaml_file_path = f'./resultados/{template}'
-    yaml_template = read_file(yaml_file_path)
-    variables = read_json('./variables.json')
-    yaml_result = replace_variables(yaml_template, variables)
-    write_file(output_yaml_file_path, yaml_result)
-    print(f"El resultado ha sido guardado en {output_yaml_file_path}")
+        templates = ["00_app.yml", "01_service.yml", "02_ingress.yml"]
+        results = []
 
+        for template in templates:
+            yaml_file_path = f'./templates/{template}'
+            output_yaml_file_path = f'./results/{template}'
+            yaml_template = read_file(yaml_file_path)
+            yaml_result = replace_variables(yaml_template, variables)
+            write_file(output_yaml_file_path, yaml_result)
+            results.append(f"El resultado ha sido guardado en {output_yaml_file_path}")
+
+        return render_template('index.html', results=results)
+
+    return render_template('index.html', results=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
